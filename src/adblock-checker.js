188944ca050e47134717df96b36e666cc2644a95
Juno_okyo <J2TeaM@users.noreply.github.com>
2015-10-24 10:50:19 +0700

    Improve

--- src/adblock-checker.js
+++ src/adblock-checker.js
@@ -1,19 +1,19 @@
 /*! AdBlock Checker v1.0.0 | (c) 2015 Juno_okyo */
 function adb_checker(config) {
     // Detect AdBlock
-    if (typeof adblock == 'undefined') {
-        if (typeof config == 'object') {
+    if (typeof adblock === 'undefined') {
+        if (typeof config === 'object') {
             var url = config.url;
-            if (typeof url != 'undefined' && url.length > 0) {
+            if (typeof url !== 'undefined' && url.length > 0) {
                 // Check redirect optional
                 if (config.redirect) {
                     window.top.location.href = url;
-                } else if (typeof(warn = config.warning) == 'object') {
+                } else if (typeof(warn = config.warning) === 'object') {
                     // Set default value
-                    if (typeof warn.text == 'undefined') {
+                    if (typeof warn.text === 'undefined') {
                         warn.text = 'Please disable AdBlock to continue!';
                     }
-                    if (typeof warn.button == 'undefined') {
+                    if (typeof warn.button === 'undefined') {
                         warn.button = 'Help me to disable!';
                     }
                     adb_warning(url, warn.text, warn.button);
@@ -27,26 +27,27 @@ function adb_checker(config) {
 }
 
 function adb_warning(url, text, button) {
-    var html = '<div class="smoke-base smoke-visible smoke-alert"><div class="smokebg"></div><div class="dialog smoke ">';
+    var html = '<div class="smoke-base smoke-visible smoke-alert"><div class="smokebg"></div><div class="dialog smoke">';
     html += '<div class="dialog-inner">' + text;
     if ((img = getHelpImg()) != -1) {
-        html += '<br /><img src="' + img + '" alt="AdBlock Checker by Juno_okyo" />';
+        html += '<br /><img src="' + img + '" alt="AdBlock Checker by Juno_okyo" style="display:block" />';
     } else {
         html += '<div class="dialog-buttons"><a href="' + url + '">' + button + '</a></div>';
     }
     html += '</div></div></div>';
+    
     var div = document.createElement('div');
     div.innerHTML = html;
     document.body.appendChild(div);
 }
 
 function getHelpImg() {
-    var ua = window.navigator.userAgent;
-    var img = 'http://i.imgur.com/';
+    var ua = window.navigator.userAgent,
+        img = document.location.protocol + '//i.imgur.com/';
 
-    if (ua.indexOf('Chrome/') != -1) {
+    if (ua.indexOf('Chrome/') > -1) {
         img += 'BOqY8vc.png';
-    } else if (ua.indexOf('Firefox/') != -1) {
+    } else if (ua.indexOf('Firefox/') > -1) {
         img += 'dOraTZG.png';
     } else {
         img = -1;
